a9dca8f860bec86b9673a008549b564155f7a186
Cynaria <Elizabeth.J.Cruz@gmail.com>
2015-07-14 12:02:18 -0500

    Resized/recolored icon

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,21 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  didInsertElement: function () {
-    var isMobile = window.innerWidth < 768;
-    if (isMobile) {
-      var navMenu = this.$('#js-navigation-menu');
-      var menuToggle = this.$('#js-mobile-menu').unbind();
-      navMenu.removeClass("show");
+  idName: ['js-navigation-menu'],
+  className: ['hide'],
+  didInsertElement: function () {
+    var navMenu = this.$('#js-navigation-menu');
+    var menuToggle = this.$('#js-mobile-menu').unbind();
+    navMenu.removeClass("show");
+    this.$('.logo').removeClass("show");
 
-      menuToggle.on('click', function(e) {
-        e.preventDefault();
-        navMenu.slideToggle(function(){
-          if(navMenu.is(':hidden')) {
-            navMenu.removeAttr('style');
-          }
-        });
-      });  
-    }
-  }
+    menuToggle.on('click', function(e) {
+      e.preventDefault();
+      navMenu.slideToggle(function(){
+        if(navMenu.is(':hidden')) {
+          navMenu.removeAttr('style');
+        }
+      });
+    });  
+  }
 });
